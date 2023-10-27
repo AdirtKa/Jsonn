f3fc048facb41a5f7a24db56ab809c5114f46634
AdirtKa <AdirtKa@yandex.ru>
2023-10-27 18:00:44 +1000

    .

--- Jsonn/Jsonn.cpp
+++ Jsonn/Jsonn.cpp
@@ -4,6 +4,57 @@
 
 using namespace nlohmann;
 
+std::vector <Planet> readPlanetsFromJson(const std::string& filename);
+void writePlanetsToJson(const std::string& filename, const std::vector <Planet>& planets);
+void printPlanetNames(const std::vector <Planet>& planets);
+Planet makePlanet();
+void printActions(const int& planetsCount);
+
+int main()
+{
+	std::string filename;
+	std::cout << "Enter a filename: "; std::cin >> filename;
+	filename = filename + ".json";
+	//filename = "planets.json";
+
+	std::vector <Planet> planets = readPlanetsFromJson(filename);
+	while (true)
+	{
+		system("cls");
+		printPlanetNames(planets);
+		int choice, planetsCount;
+		planetsCount = planets.size();
+		printActions(planetsCount);
+		std::cout << "Enter a action: "; std::cin >> choice;
+		if (choice < 0 || choice > planetsCount + 3) {
+			std::cout << "Invalid action" << std::endl; 
+			continue;
+		}
+		if (choice == 0) {
+			return 0;
+		}
+		else if (choice == planetsCount + 1) {
+			planets.push_back(makePlanet());
+		}
+		else if (choice == planetsCount + 2) {
+			int planetToEraseIndex;
+			std::cout << "Which planet do you want to delete? "; std::cin >> planetToEraseIndex;
+			planets.erase(planets.begin() + planetToEraseIndex - 1);
+		}
+		else if (choice == planetsCount + 3) {
+			writePlanetsToJson(filename, planets);
+		}
+		else if (choice < planetsCount + 1) {
+			planets[choice - 1].print_planet();
+			system("pause");
+		}
+	}
+
+	writePlanetsToJson(filename, planets);
+
+	return 0;
+}
+
 std::vector <Planet> readPlanetsFromJson(const std::string& filename) {
 	std::ifstream file(filename);
 	std::vector <Planet> planets;
@@ -72,49 +123,4 @@ void printActions(const int& planetsCount) {
 	std::cout << planetsCount + 1 << ". Add a planet" << std::endl;
 	std::cout << planetsCount + 2 << ". Remove a planet" << std::endl;
 	std::cout << planetsCount + 3 << ". Save the planets to a json file" << std::endl;
-}
-
-int main()
-{
-	std::string filename;
-	std::cout << "Enter a filename: "; std::cin >> filename;
-	filename = filename + ".json";
-	//filename = "planets.json";
-
-	std::vector <Planet> planets = readPlanetsFromJson(filename);
-	while (true)
-	{
-		system("cls");
-		printPlanetNames(planets);
-		int choice, planetsCount;
-		planetsCount = planets.size();
-		printActions(planetsCount);
-		std::cout << "Enter a action: "; std::cin >> choice;
-		if (choice < 0 || choice > planetsCount + 3) {
-			std::cout << "Invalid action" << std::endl; 
-			continue;
-		}
-		if (choice == 0) {
-			return 0;
-		}
-		else if (choice == planetsCount + 1) {
-			planets.push_back(makePlanet());
-		}
-		else if (choice == planetsCount + 2) {
-			int planetToEraseIndex;
-			std::cout << "Which planet do you want to delete? "; std::cin >> planetToEraseIndex;
-			planets.erase(planets.begin() + planetToEraseIndex - 1);
-		}
-		else if (choice == planetsCount + 3) {
-			writePlanetsToJson(filename, planets);
-		}
-		else if (choice < planetsCount + 1) {
-			planets[choice - 1].print_planet();
-			system("pause");
-		}
-	}
-
-	writePlanetsToJson(filename, planets);
-
-	return 0;
 }
